703b3c756b686d1ce8355d344376759d5bca334e
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-14 23:29:18 +0300

    number

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("Hello world");
+/*типы данных:
+* 1. Примитивные
+* - Number: 2021, 1.5, NaN, Infinity
+* - String: 'Hello', "hello", `hello`
+* - Boolean: true, false
+* - Null: null
+* - Undefined: undefined,
+* - Symbol()
+*/
+
+// NUMBER
+console.log(2021, 1.5);
+console.log(20 * 'asdfdsss');
+console.log(1 / 0);
+
+// STRING
