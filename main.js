012fa4a3cb503dc8a10fdf0885f73545b19a9805
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-16 23:26:00 +0300

    to string

--- main.js
+++ main.js
@@ -1,49 +1,24 @@
-/*типы данных:
-* 1. Примитивные
-* - Number: 2021, 1.5, NaN, Infinity
-* - String: 'Hello', "hello", `hello`
-* - Boolean: true, false
-* - Null: null
-* - Undefined: undefined,
-* - Symbol()
-*/
+let value;
 
-// NUMBER
-console.log(2021, 1.5);
-console.log(20 * 'asdfdsss');
-console.log(1 / 0);
+// Number to string
+value = String(10);
+value = String(10+40);
+value = (40).toString();
 
-// STRING
-console.log('Hello', "Hello", `Hello`);
+//Boolean to string
+value = String(false);
 
-// BOOLEAN
-console.log(true, false);
+// Array to string
+value = String([1, 2, 3]);
 
-// NULL
-console.log(null);
+// Object to string
+value = String({ name: 'Alex'});
 
-// UNDEFINED
-console.log(undefined);
+// Concatination
+value = 30 + '' + 30;
+value = 30 - 'asdsda';
+value = 30 - '5';
+value = true + 10;
 
-//SYMBOL
-console.log(Symbol());
-
-/**
- * Типы данных:
- * 2. Обьекты. (reference type) Передаются по ссылке.
- * - Object: { name: 'Alex', age: 30 }
- *   * Array: [1, 2, 3]
- *   * Function: function foo() {}
- *   * Date: new Date()
- *  ....
- */
-console.table({ name: 'Alex', age: 30 });
-console.log([1, 2, 3, 4]);
-
-/**
- * Особенности:
- * 1. JS динамически типизированный язык
- * 2. Одна и та же переменная может храниить в себе любой тип данных
- * 3. Нам не нужно определять тип данных при создании переменных
- * 4. Если мы хотим использовать статическую типизацию мы можем использовать Typescript или Flow
- */
+console.log(value);
+console.log(typeof value);
