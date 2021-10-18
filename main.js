a766f3fe4ff887aeb6f5287e9f235ec712743059
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-19 00:39:53 +0300

    1

--- main.js
+++ main.js
@@ -1,30 +1,17 @@
 const firstName = 'Alex';
 const lastName = 'Bond';
 const age = 30;
-const str = 'Hello my name is Alex';
 
-let value;
-value = firstName + lastName;  // конкатенация - сложение строк
-value = firstName + ' ' + lastName;
-// value = value + ' I am ' + age;
-value += ' I am ' + age;
+let str;
 
-// properties
-value = firstName.length;
-value = firstName[2];
-value = firstName[4];
-value = lastName[lastName.length - 1];
+str = 'Hello my name is ' + firstName + ' ' + lastName;
 
-// methods
-value = firstName.toUpperCase();  // lowerCase - понижает регистр
-value = firstName.concat(' ', lastName);  // concat - конкатенация
-value = firstName.indexOf('e');  // возвращает индекс первого вхождения данной строки 
-value = str.indexOf('my');
-// lastIndexOf - тоже самое, только с конца строки
-value = str.includes('Alex'); // возвращает булево значение на предмет наличия выражения в строке
+str = '<ul>' +
+    '<li>First name: ' + firstName + '</li>' +
+    '<li>Last name: ' + lastName + '</li>' +
+    '<li>Age: ' + age + '</li>' +
+    '</ul>';
 
-value = str.slice(0, 8);  //метод взятия подстроки из строки (вырезать что-то)
-value = str.slice(0, -4);  // меняет
-value = str.replace('Alex', 'Oleks');
+// console.log(str);
 
-console.log(value);
+document.body.innerHTML = str;
