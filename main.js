d1f375ddb0873b8b30efdaa5e1d824d7e1e6519b
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-20 22:50:42 +0300

    обращение к свойствам объекта

--- main.js
+++ main.js
@@ -8,5 +8,20 @@ const user = {
     },
     skills: ['html', 'css', 'js']
 };
+ 
+// чтобы обратиться к свойствам обьекта есть два способа
+// 1й способ: обратиться через точку
+let value;
+let prop = 'email';
 
+value = user.firstName;
+// 2й способ: обратиться через [ ]
+value = user['isAdmin']; // используется для специфичных ключей ('user-address'), т.к. через точку их получить не получиться
+value = user['user-address'];
+// чтобы получить вложеные свойства в обьект мы можем продолжить эту цепочку
+value = user['user-address'].city;  // либо через точку
+value = user['user-address']['city'];  // либо через квадратные скобки
+value = user[prop];
+
+console.log(value);
 console.log(user);
