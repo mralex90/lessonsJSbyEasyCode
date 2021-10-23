fd3ef76f2bf3654f63f14a0db6e1d7c39291aea1
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-23 22:29:22 +0300

    тернарный оператор

--- main.js
+++ main.js
@@ -1,118 +1,14 @@
-// >, <, >=, <=, ==, ===, !=, !==   -  операторы сравнения
+let a = 1;
+let b = 0;
 
-let value;
-
-value = 1 > 2;
-value = 2 <= 2;
-value = 1 == 1;
-value = 1 == '1';  // не строгое равенство(==) не проверяет на тип данных и автоматически преобразует значение в число
-value = 1 === '1';  // другая ситуация, т.к. типы данных у них разные
-value = 1 != '1';  // оператор неравенства не обращ внимание на типы
-value = 1 !== '1';  // строгое неравенство, учитывает типы
-value = 'a' > 'a';
-value = 'a' > 'ab';
-// чтобы посмотреть к какому юникоду относится какой символ:
-value = 'a'.charCodeAt();
-value = 'A'.charCodeAt();
-
-// console.log(value);
-
-// /* if (условие) {
-//     пишется код, котор выполнится если условие будет правдивым
-    
-//     action
-
+// if (a > 0) {
+//     b = a;
 // } else {
-//     если это условие не выполнится, то сработает конструкция else ( не обязательна)
-
-//     else action
-// }
-// */
-// if (условие/выражение/переменная/вычисления) {
-//     //action   если здесь будет "true" то действие выполнится, если нет - выполнится "else"
-// } else {
-//     // else action
+//     b += 1;
 // }
 
-value = 10;
+// синтаксис: выражение ? если true : если false;
 
-/*if (value !== 10) {  // старайся делать строгое сравнение
-    console.log('value === 10');
-} else {
-    console.log('else');
-}*/
+b = a > 0 ? b = a : b += 1;
 
-// Логические операторы:
-// || (или), && (и), ! (логическое не)
-
-// value = null;  // null в булевом значении это 'false'
-
-// console.log(!value);  // а '!' преобразует в противоположное будлево значение от текущего
-
-// if (value === null) {
-//     console.log(value);
-// }
-
-// value = [1]; // даже пустой массив == 'true'
- 
-// if (value.length) {
-//     console.log(value);
-// } else {
-//     console.log('array is empty');
-// }
- // проверка что массив является массивом
-//  value = null;
-//  if (Array.isArray(value)) {
-//      console.log(value);
-//  } else {
-//      console.log('array is empty');
-//  }
-
-// let user = {
-//     name: 'Alex'
-// };
-
-// if (user.hasOwnProperty('name')) { // hasOwnProperty этот метод возвращает true/false, принимает имя ключа обьекта и говорит есть или нет. Но он не проверяет  значение этого свойства
-//     console.log(user.name);
-// } else {
-//     console.log('else');
-// }
- 
-// оператор "или" (||) запинается (пытается преобразовать все выражения) на False
-// оператор "и" (&&) запинается (пытается преобразовать в true) на True
-// value = 0 || 1;
-// value = 0 || 0 || null;  // возвращает последний false
-
-// let age = 10;
-
-// if (age < 16 || age > 65) {
-//     console.log('some action');
-// } else {
-//     console.log('else');
-// }
-
-// let serverNickname = 'Alex';
-// let nickname = serverNickname || 'default nickname';
-
-// console.log(nickname);
-
-// value = 1 && 0 && 3;
-
-// productPrice = 10;
-// if(productPrice >= 5 && productPrice <= 20) {
-//     console.log('беру');
-// } else {
-//     console.log('else');
-// }
-
-// console.log(value);
-
-value = 10;
-
-if (value < 10) {  // if и else, если одна строка, можна записать без {}
-    console.log('value < 10', value);
-} else if (value >= 10) {
-    console.log('value >=10', value);
-} else {  // кострукций if/else может быть множество, но рекомендуется больше 3х
-    console.log('else');  // нужно стараться уменьшать кол-во условий - они затрудняют код тестировать, расширять, изменять
-}
+console.log(`b: ${b}`);
