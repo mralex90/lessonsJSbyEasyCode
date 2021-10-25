9f235f03f32119d168ade70958d96c2e524d53dc
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-25 23:37:27 +0300

    области видимости ф-ии

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ function sayHello(firstName = "Default", lastName = "Default") {
     console.log("Hello World");
     return `Hello ${firstName} ${lastName}`;
 }
+
 //  let res = sayHello("Oleks", "Bondarenko");
 //  let res2 = sayHello("Aleksandr", "Bondarenko") + "!";
- let res3 = sayHello(null);
- console.log(res3);
+//  let res3 = sayHello(null);
+//  console.log(res3);
+
+//области видимости ф-ии
+
+// let x = 10; 
+
+// function foo(x) {
+//     x = 20;
+//     console.log(x);
+//     return x;   
+// }
+
+// foo();
+
+// console.log(x);
+
+// const user = {
+//     name: 'Oleks',
+//     age: 30
+// };
+
+// function getObj(obj) {
+//     console.log(obj);
+//     obj.name = "Aleks";
+// }
+
+// getObj(user);
+
