0e7598faf65d546130c192362350573565cd47ae
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-25 23:24:51 +0300

    значение аргумента ф-ии по умолчанию

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function sayHello(firstName, lastName) {
+function sayHello(firstName = "Default", lastName = "Default") {
     console.log(firstName, lastName);
     console.log("Hello World");
     return `Hello ${firstName} ${lastName}`;
 }
- let res = sayHello("Oleks", "Bondarenko");
- let res2 = sayHello("Aleksandr", "Bondarenko") + "!";
- console.log(res2);
+//  let res = sayHello("Oleks", "Bondarenko");
+//  let res2 = sayHello("Aleksandr", "Bondarenko") + "!";
+ let res3 = sayHello(null);
+ console.log(res3);
